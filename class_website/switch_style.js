5af7a95944b2824d7d55ecd93eea76fd51fdc66c
Ruthie Byers <ruthie@mit.edu>
2013-11-19 11:16:03 -0500

    class website and student sample style

---
+++ class_website/switch_style.js
@@ -0,0 +1,16 @@
+change_style = function(filename) {
+    document.getElementById("style").href = filename
+} 
+
+/*yazi = function() {
+    document.getElementById("style").href ="yazi.css"
+}
+
+devin = function () {
+    document.getElementById("style").href = "devin.css"
+}
+
+none = function() {
+    document.getElementById("style").href ="hw/template_stylesheet.css"
+}*/
+
